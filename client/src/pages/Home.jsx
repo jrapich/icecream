30f8086532101b1803416d63d9873536e4175e38
Jeremy Rapich <jeremysr@protonmail.com>
2024-02-22 18:15:15 -0700

    added logic and state for determining percentage of page scroll, to use to transition in elements based on percentage of page scrolled

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   Box,
   Container,
@@ -10,15 +11,41 @@ import {
   CardContent,
   CardMedia,
   Divider,
+  Slide,
 } from "@mui/material";
 import theme from "../theme";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Home() {
+  const [scrollTop, setScrollTop] = useState(0);
+  const [showContent, setShowContent] = useState(false);
+
+  const handleScroll = () => setScrollTop(window.scrollY);
+  
+
+  useEffect(() => {
+    const windowHeight = window.innerHeight;
+    const pageHeight = document.documentElement.scrollHeight;
+    const actualHeight = pageHeight - windowHeight;
+    const scrollProgress = 100 * (scrollTop / actualHeight);
+    console.log("scrolltop:", scrollTop);
+    console.log("window height:", windowHeight, "page height:", pageHeight);
+    console.log("actual height:", actualHeight);
+    console.log("percent scrolled:", scrollProgress);
+    window.addEventListener("scroll", handleScroll);
+    
+    if (scrollProgress > 30) {
+      setShowContent(true);
+    }
+    
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollTop]);
   return (
     <>
-      <section>
+      <section style={{
+        height: 4000,
+      }} >
         <Box
           sx={{
             pt: 4,
@@ -63,13 +90,13 @@ export default function Home() {
               </Typography>
               <Typography
                 variant="h6"
-                sx={{ px: 2, color: theme.palette.background.main}}
+                sx={{ px: 2, color: theme.palette.background.main }}
               >
                 background color &quot;persian pink&quot;
               </Typography>
               <Typography
                 variant="h6"
-                sx={{ px: 2, color: theme.palette.accent.main}}
+                sx={{ px: 2, color: theme.palette.accent.main }}
               >
                 accent color &quot;russian violet&quot;
               </Typography>
@@ -91,50 +118,58 @@ export default function Home() {
         <Divider component="div" role="presentation">
           <Typography>fancy divider</Typography>
         </Divider>
-        <Container
-          sx={{
-            paddingTop: 2,
-            paddingBottom: 4,
-          }}
-          maxWidth="md"
+        <Slide
+          direction="left"
+          in={showContent}
+          mountOnEnter
+          unmountOnExit
+          {...(showContent ? { timeout: 1000 } : { timeout: 500 })}
         >
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    border: 0.5,
-                    borderRadius: 10,
-                    borderColor: theme.palette.tertiary.main,
-                  }}
-                >
-                  <CardMedia
-                    component="div"
+          <Container
+            sx={{
+              paddingTop: 2,
+              paddingBottom: 4,
+            }}
+            maxWidth="md"
+          >
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
                     sx={{
-                      // 16:9
-                      pt: "56.25%",
-                      backgroundSize: "cover",
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      border: 0.5,
+                      borderRadius: 10,
+                      borderColor: theme.palette.tertiary.main,
                     }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Card Name
-                    </Typography>
-                    <Typography>Card Description</Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">view</Button>
-                    <Button size="small">edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+                  >
+                    <CardMedia
+                      component="div"
+                      sx={{
+                        // 16:9
+                        pt: "56.25%",
+                        backgroundSize: "cover",
+                      }}
+                      image="https://source.unsplash.com/random?wallpapers"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Card Name
+                      </Typography>
+                      <Typography>Card Description</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">view</Button>
+                      <Button size="small">edit</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </Slide>
       </section>
     </>
   );
