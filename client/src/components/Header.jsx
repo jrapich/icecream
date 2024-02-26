13b02e528904f41c54bf9e4416e6e6d6c3ff4fb3
Jeremy Rapich <jeremysr@protonmail.com>
2024-02-26 16:17:00 -0700

    removed slide effect from header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,17 +3,14 @@ import {
   Toolbar,
   Typography,
   Container,
-  Slide,
   Box,
 } from "@mui/material";
 
 export default function Header() {
-  
-
   return (
     <>
       <Box
-      component={"header"}
+        component={"header"}
         sx={{
           position: "sticky",
           top: 0,
@@ -21,17 +18,15 @@ export default function Header() {
           zIndex: 1,
         }}
       >
-        <Slide direction="down" in={true} {...{ timeout: 1500 }}>
-          <AppBar component={"div"} position="relative">
-            <Toolbar>
-              <Container>
-                <Typography variant="h6" color="inherit" align="center" noWrap>
-                  <i>BJ&apos;s Ice Cream</i>
-                </Typography>
-              </Container>
-            </Toolbar>
-          </AppBar>
-        </Slide>
+        <AppBar component={"div"} position="relative">
+          <Toolbar>
+            <Container>
+              <Typography variant="h6" color="inherit" align="center" noWrap>
+                <i>BJ&apos;s Ice Cream</i>
+              </Typography>
+            </Container>
+          </Toolbar>
+        </AppBar>
       </Box>
     </>
   );
