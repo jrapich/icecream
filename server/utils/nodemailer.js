de1f70eb8b37f55c5c720d8c7c137a8da7ace283
Jeremy Rapich <jeremysr@protonmail.com>
2024-02-27 14:39:50 -0700

    pseudocode for nodemailer

---
+++ server/utils/nodemailer.js
@@ -0,0 +1,13 @@
+//import nodemailer
+//import anything else we need for nodemialer
+
+//define vars needed for nodemailer
+//is this accounts: IMAP info? auth? etc
+
+//create necessary objects/classes/functions for nodemailer to operate
+
+//create an exportable function for handling nodemailer data as arguments
+//and return either a fufilled or reject promise based on:
+//1 if the needed data was passed,
+//2 if the request passed to nodemailer was successful or not.
+//consider wrapping all of this with OOP into an exportable class, if possible
