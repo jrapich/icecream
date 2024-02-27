5566a29aa3d2632fc876547da555f061f48b857f
Jeremy Rapich <jeremysr@protonmail.com>
2024-02-27 16:41:53 -0700

    added array of images for, which might later be dynamicly populated from /public/assets

---
+++ client/src/utils/imageList.js
@@ -0,0 +1,26 @@
+// import these when app is built in production
+// import one from "/assets/1-bjs-logo.jpg";
+// import two from "/assets/2-icecream.jpeg";
+// import three from "/assets/3-icecream.JPG";
+// import four from "/assets/4-icecream.jpg";
+// import five from "/assets/5-Bjs_Sign.webp";
+// import six from "/assets/6-icecream.JPG";
+
+//use these strings when app is in development with vite
+const one = "/assets/1-bjs-logo.jpg";
+const two = "/assets/2-icecream.jpeg";
+const three = "/assets/3-icecream.JPG";
+const four = "assets/4-icecream.jpg";
+const five = "/assets/5-Bjs_Sign.webp";
+const six = "/assets/6-icecream.JPG";
+
+const imageList = [
+  { img: one, title: "bjs logo" },
+  { img: two, title: "icecream" },
+  { img: three, title: "icecream" },
+  { img: four, title: "icecream" },
+  { img: five, title: "bjs sign" },
+  { img: six, title: "icecream" },
+];
+
+export default imageList;
