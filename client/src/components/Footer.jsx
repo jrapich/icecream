810749ccd4fbb8c235c23f477c950383079e4f33
Jeremy Rapich <jeremysr@protonmail.com>
2024-03-04 16:39:25 -0700

    footer edits

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,28 +2,28 @@ import { Link, Box, Typography } from "@mui/material";
 
 function Copyright() {
   return (
-    <Typography variant="body2" align="center">
-      {"Copyright © "}
-      BJ&apos;s Ice Cream {new Date().getFullYear()}
-      <Link color="inherit" variant="inherit" underline="none" href="https://mui.com/">
-        <Typography variant="subtitle2" align="center">powered by React + Material UI </Typography>
+    <Box>
+      <Typography variant="body2" align="center">
+        {"Copyright © "} BJ&apos;s Ice Cream {new Date().getFullYear()}
+      </Typography>
+      <Link
+        color="inherit"
+        variant="inherit"
+        underline="none"
+        href="https://mui.com/"
+      >
+        <Typography variant="subtitle2" align="center">
+          powered by React + Material UI{" "}
+        </Typography>
       </Link>{" "}
-    </Typography>
+    </Box>
   );
 }
 
 export default function Footer() {
   return (
-    <>
-      <Box sx={{ p: 6 }} component="footer">
-        {/* <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" component="p">
-          Something here to give the footer a purpose!
-        </Typography> */}
-        <Copyright />
-      </Box>
-    </>
+    <Box sx={{ p: 6 }} component="footer">
+      <Copyright />
+    </Box>
   );
 }
